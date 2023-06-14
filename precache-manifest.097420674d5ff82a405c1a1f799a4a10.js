0dbc22748c7b9b357665a3ff8676ee2e31fb1cb3
Irtaza Naqvi <irtaza@mira-ig.de>
2023-06-14 22:02:53 +0500

    Updates

---
+++ precache-manifest.097420674d5ff82a405c1a1f799a4a10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bafdb8f1a237a568d0539773d2aafb63",
+    "url": "/irtaza1234/bleota/index.html"
+  },
+  {
+    "revision": "6bcdcf45b6c46de11f6d",
+    "url": "/irtaza1234/bleota/static/css/main.7e6de23a.chunk.css"
+  },
+  {
+    "revision": "63434a638cabb531d86c",
+    "url": "/irtaza1234/bleota/static/js/2.ddd56c7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/irtaza1234/bleota/static/js/2.ddd56c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bcdcf45b6c46de11f6d",
+    "url": "/irtaza1234/bleota/static/js/main.06569a6d.chunk.js"
+  },
+  {
+    "revision": "e7af1eec742a369e1e17",
+    "url": "/irtaza1234/bleota/static/js/runtime-main.5ef9f445.js"
+  }
+]);
